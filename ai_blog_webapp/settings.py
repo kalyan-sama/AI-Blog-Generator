9c76db6586b1d2e33ca56350e5d814a32fb88263
Kalyan <kalyannagavenkat123@gmail.com>
2024-04-21 18:32:15 +0530

    update settings.py

--- ai_blog_webapp/settings.py
+++ ai_blog_webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-px2x&icrjzg$mc*-=iimr^ey=k_b9hr+kyyjv2j7@&@%b8n11f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-blog-generator.pages.dev']
 
 
 # Application definition
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ai_blog_webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
