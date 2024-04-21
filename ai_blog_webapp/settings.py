8ef9c873d980c926c675353c363b59a33d0014ce
Kalyan <kalyannagavenkat123@gmail.com>
2024-04-21 21:10:21 +0530

    update ALLOWED_HOSTS

--- ai_blog_webapp/settings.py
+++ ai_blog_webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-px2x&icrjzg$mc*-=iimr^ey=k_b9hr+kyyjv2j7@&@%b8n11f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ai-blog-generator-production.up.railway.app']
 
 
 # Application definition
