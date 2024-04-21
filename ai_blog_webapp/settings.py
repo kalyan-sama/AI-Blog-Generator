b0d3505efbd959acc18a035d6a7e2abdcbfc4002
Wahid Ali <aliwahid1716@gmail.com>
2024-04-21 23:56:34 +0530

    Update .gitignore and settings.py

--- ai_blog_webapp/settings.py
+++ ai_blog_webapp/settings.py
@@ -149,4 +149,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT =os.path.join(BASE_DIR, 'static')
